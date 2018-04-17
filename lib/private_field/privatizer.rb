2c3bb471da38904d2fd921808f04df4a5c65a274
Robert Ingrum <rkingrum@gmail.com>
2018-04-17 01:06:53 -0600

    This probably isn't the right way to do this but I really just need to change this one method

--- lib/private_field/privatizer.rb
+++ lib/private_field/privatizer.rb
@@ -30,3 +30,20 @@ module PrivateField
     end
   end
 end
+
+module Mongoid::Attributes::Processing
+  private
+
+  def process_attribute(name, value)
+    method_name = "#{name_alias(name) || name}="
+
+    return send(method_name, value)                          if respond_to? method_name
+    return Errors::PrivateField.new(self.class, method_name) if respond_to? method_name, true
+
+    raise Errors::UnknownAttribute.new(self.class, name)
+  end
+
+  def name_alias(name)
+    aliased_fields.invert[name.to_s]
+  end
+end
